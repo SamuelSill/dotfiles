184fc9ab6b13937257b41962d6663b6f70e7198f
muli <muli.silman@island.io>
2026-07-24 13:15:12 +0300

    Add default editor/visual

--- setup.zsh
+++ setup.zsh
@@ -3,6 +3,7 @@
 DOTFILES_DIR="${0:A:h}"
 export DOTFILES_DIR
 
+source "$DOTFILES_DIR/setup/env.zsh"
 source "$DOTFILES_DIR/setup/oh-my-zsh.zsh"
 source "$DOTFILES_DIR/setup/pyenv.zsh"
 source "$DOTFILES_DIR/setup/fzf.zsh"
