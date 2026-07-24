9dfeac2059cadea89d8a77e2942b1ebd2769be63
muli <muli.silman@island.io>
2026-07-24 15:23:24 +0300

    Improve zsh prompt to be cleaner and show live time

--- setup/oh-my-zsh.zsh
+++ setup/oh-my-zsh.zsh
@@ -2,4 +2,15 @@ export ZSH="$HOME/.oh-my-zsh"
 ZSH_THEME="robbyrussell"
 plugins=(git zsh-syntax-highlighting zsh-autosuggestions)
 source "$ZSH/oh-my-zsh.sh"
-RPROMPT="[%D{%H:%M:%S}]"
+
+PROMPT='%{$fg[yellow]%}[%*]%{$reset_color%} %{$fg_bold[cyan]%}%c%{$reset_color%} $(git_prompt_info)
+%(?:%{$fg_bold[green]%}❯%{$reset_color%} :%{$fg_bold[red]%}❯%{$reset_color%} )'
+RPROMPT=""
+
+ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg[magenta]%}"
+ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
+ZSH_THEME_GIT_PROMPT_DIRTY=" %{$fg[yellow]%}✗%{$reset_color%}"
+ZSH_THEME_GIT_PROMPT_CLEAN=""
+
+TMOUT=1
+TRAPALRM() { zle reset-prompt }
