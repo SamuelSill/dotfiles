f8bcb24f71a5bb116494df97e9b0e8e95802283b
muli <muli.silman@island.io>
2026-08-02 14:23:49 +0300

    Improve git prompt info in zsh prmopt

--- setup/oh-my-zsh.zsh
+++ setup/oh-my-zsh.zsh
@@ -3,12 +3,12 @@ ZSH_THEME="robbyrussell"
 plugins=(git zsh-syntax-highlighting zsh-autosuggestions)
 source "$ZSH/oh-my-zsh.sh"
 
-PROMPT='%{$fg[yellow]%}[%*]%{$reset_color%} %{$fg_bold[cyan]%}%c%{$reset_color%} ($(git_prompt_info))
+PROMPT='%{$fg[yellow]%}[%*]%{$reset_color%} %{$fg_bold[cyan]%}%c%{$reset_color%}$(git_prompt_info)
 %(?:%{$fg_bold[green]%}❯%{$reset_color%} :%{$fg_bold[red]%}❯%{$reset_color%} )'
 RPROMPT=""
 
-ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg[magenta]%}"
-ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
+ZSH_THEME_GIT_PROMPT_PREFIX=" (%{$fg[magenta]%}"
+ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%})"
 ZSH_THEME_GIT_PROMPT_DIRTY=" %{$fg[yellow]%}✗%{$reset_color%}"
 ZSH_THEME_GIT_PROMPT_CLEAN=""
 
